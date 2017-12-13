311951865eda89901fc0a3397d05b4e0bc96fbea
Elena <elenacerezo83@gmail.com>
2017-12-13 03:32:33 +0100

    first

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+var userName = prompt('¿Cómo te llamas?');
+
+if (userName == 'María' || userName == 'Elena') {
+alert('Bienvenida, '+ userName);
+}else {
+alert('el usuario no está registrado en esta página');
+
+}
