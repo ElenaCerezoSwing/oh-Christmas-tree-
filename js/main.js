34c3f957f37ab6a267cee90f5897e34794483fd4
Elena <elenacerezo83@gmail.com>
2017-12-13 04:01:57 +0100

    for solution to doggy exercise

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@
 var dogAge = parseInt(prompt('¿cuántos años tiene tu perro?'));
 var humanAge = 0;
 
-if (dogAge === 1) {
-humanAge += 15;
+for (var i = 1 ; i <= dogAge; i++) {
+  if (i === 1) {
+humanAge = humanAge + 15;
   }
-else if (dogAge === 2) {
-humanAge +=24;
+else if (i === 2) {
+humanAge = humanAge + 9;
   }
 else {
-  humanAge = (dogAge-2)*5 + 24;
+  humanAge = humanAge + 5;
 }
-
+};
 alert('tu perro tiene ' + dogAge + ' años perrunos que equivalen a ' + humanAge + ' años humanos :)');
