0974286b72d96d3912ae7614bae316b446f3608d
Elena <elenacerezo83@gmail.com>
2017-12-13 03:46:17 +0100

    doggy

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
 
-var userName = prompt('¿Cómo te llamas?');
+var dogAge = parseInt(prompt('¿cuántos años tiene tu perro?'));
+var humanAge = 0;
 
-if (userName == 'María' || userName == 'Elena') {
-alert('Bienvenida, '+ userName);
-}else {
-alert('el usuario no está registrado en esta página');
-
+if (dogAge === 1) {
+humanAge += 15;
+  }
+else if (dogAge === 2) {
+humanAge +=24;
+  }
+else {
+  humanAge = (dogAge-2)*5 + 24;
 }
+
+alert('tu perro tiene ' + dogAge + ' años perrunos que equivalen a ' + humanAge + ' años humanos :)');
