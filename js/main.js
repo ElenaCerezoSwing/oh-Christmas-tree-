414a4d48c72a057085d93146677c96a3e283034a
Elena <elenacerezo83@gmail.com>
2017-12-13 04:26:37 +0100

    tree with star and a tree trunk

--- js/main.js
+++ js/main.js
@@ -4,13 +4,18 @@
 var treeHeight = parseInt(prompt('¿cuántos pisos tendrá tu árbol de Navidad?'));
 var treeDraw = '▲';
 var counter = '';
+var star = '★';
+var treeTrunk = '|';
 
+counter = star + '\n';
 
 for (var i = 0 ; i < treeHeight; i++) {
 for (var j = 0; j <= i; j++) {
   counter = counter + treeDraw;
 }
 counter = counter + '\n';
+
 }
+counter = counter + treeTrunk;
 
 alert(counter);
